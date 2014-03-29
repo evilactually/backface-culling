20e192451205ad2cf22c8c46dbe166b3c6de385b
Vlad Sarella <vlad.shcherbakov@gmail.com>
2014-03-28 18:38:26 -0600

    Streamlining many functions

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-
-function load_model(url)
-{
-  var r = new XMLHttpRequest();
-  r.open("get", url, false);
-  r.send();
-  if(window.JSON.parse)
-  {
-    return window.JSON.parse(r.responseText);
-  }
-  else
-  {
-    console.debug("Your browser doesn't support native JSON parsing, go away.");
-    return null;
-  }
-}
-
 /**
  * Provides requestAnimationFrame in a cross browser way.
  */
@@ -41,7 +24,74 @@ window.cancelRequestAnimFrame = (function() {
                                  window.clearTimeout;
                                  })();
 
+/*
+  Vector addition 
+ */
+Array.prototype.v_add_v = function(b) {
+  var a = this;
+  var c = [];
+  for (var i = 0; i < a.length; i++) {
+    c[i] = a[i] + b[i];
+  }
+  return c;
+};
 
+Array.prototype.m_cols = function(m) {
+  if(this[0] instanceof Array)
+    return this[0].length;
+  else
+    console.debug("Not a matrix");
+};
+
+Array.prototype.m_rows = function(m) {
+  if(this instanceof Array)
+    return this.length;
+  else
+    console.debug("Not a matrix");
+};
+
+/* 
+  Same as m_mul_m, but accepts a vector in transpose notation on the right,
+  and returns transformed vector in transpose notation back(for convenience)
+ */
+Array.prototype.m_mul_v = function(b) { 
+  return this.m_mul_m([b].m_transpose()).m_transpose()[0];
+};
+
+/* 
+  Matrix multiplication
+ */
+Array.prototype.m_mul_m = function(b) {
+  var a = this;
+  var c = [[]];
+  var rows = this.m_rows();
+  var cols = this.m_cols();
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      c[i] = c[i] || new Array(cols);
+      c[i][j] = 0;
+      for (var k = 0; k < rows; k++) {
+        c[i][j] += a[i][k]*b[k][j];
+      };
+    };
+  };
+  return c;
+};
+
+Array.prototype.m_transpose = function(m) {
+  var rows = this.length;
+  var cols = this[0].length;
+  var mt = [[]];
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      if(i==0) mt[j] = [];
+      mt[j][i] = this[i][j];
+    }
+  }
+  return mt;
+};
+
+// Vector, Vector -> Vector
 function vector_add(a,b)
 {
   var c = [];
@@ -51,23 +101,26 @@ function vector_add(a,b)
   return c;
 }
 
+// Matrix, Vector -> Vector
 function vector_xfrom(m,v) {
   return matrix_transpose(matrix_mul(m, matrix_transpose([v])))[0];
 }
 
+// Matrix -> Matrix
 function matrix_transpose(a) {
   var rows = a.length;
   var cols = a[0].length;
   var b = [[]];
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < cols; j++) {
-      b[j] = b[j] || new Array(cols);
+      if(i==0) b[j] = [];
       b[j][i] = a[i][j];
     }
   }
   return b;
 }
 
+// Matrix, Matrix -> Matrix
 function matrix_mul(a,b) {
   var rows = a.length;
   var cols = a[0].length;
@@ -82,6 +135,14 @@ function matrix_mul(a,b) {
     };
   };
   return c;
+}
+
+// Number -> Matrix
+function Scale(a) {
+  return [[a, 0, 0, 0],
+          [0, a, 0, 0],
+          [0, 0, a, 0],
+          [0, 0, 0, 1]];
 }
 
 // Vector -> Matrix
@@ -119,6 +180,7 @@ function RotationZ(a) {
           [0, 0, 0, 1]];
 }
 
+// [Vector], Integer -> Vector
 function median(vs, d) {
   var m = [];
   for (var i = 0; i < d; i++) {
@@ -131,18 +193,32 @@ function median(vs, d) {
   return m;
 }
 
+// (a->a), [a] -> [a] 
+function map(f, xs) {
+    var xs_out = [];
+    xs.forEach(function (x) {
+        xs_out[xs_out.length] = f(x);
+    });
+    return xs_out;
+}
+
+/*
+  Return fall-back value if first value is undefined or null
+*/
+function Fallback(a, b) {
+  return (typeof a !== 'undefined' && a !== null) ? a : b;
+}
+
+
 /*
   CCW check based on reflection parity, requires points to be in screen or ndc space.
 
   [Vector, Vector, Vector] -> Bool */
 function isCCW2D(vs)
 {
-  var x0 = vs[0][0];
-  var y0 = vs[0][1];
-  var x1 = vs[1][0];
-  var y1 = vs[1][1];
-  var x2 = vs[2][0];
-  var y2 = vs[2][1];
+  var x0 = vs[0][0]; var y1 = vs[1][1];
+  var y0 = vs[0][1]; var x2 = vs[2][0];
+  var x1 = vs[1][0]; var y2 = vs[2][1];
   var d = x2*(y0-y1)+x0*(y1-y2)+x1*(-y0+y2);
   return d < 0; // expecting a single reflection, because y-axis is pointing down in screen coordinates
 }               // otherwise we would be expecting a positive value
@@ -154,20 +230,36 @@ function isCCW2D(vs)
   [Vector, Vector, Vector] -> Bool */
 function isCCW3D(vs)
 {
-  var x0 = vs[0][0];
-  var y0 = vs[0][1];
-  var z0 = vs[0][2];
-  var x1 = vs[1][0];
-  var y1 = vs[1][1];
-  var z1 = vs[1][2];
-  var x2 = vs[2][0];
-  var y2 = vs[2][1];
-  var z2 = vs[2][2];
+  var x0 = vs[0][0]; var x1 = vs[1][0]; var x2 = vs[2][0];
+  var y0 = vs[0][1]; var y1 = vs[1][1]; var y2 = vs[2][1];
+  var z0 = vs[0][2]; var z1 = vs[1][2]; var z2 = vs[2][2];  
   var d = x2*y1*z0-x1*y2*z0-x2*y0*z1+x0*y2*z1+x1*y0*z2-x0*y1*z2;
   return d < 0;
 }
 
+function load_model(url)
+{
+  var r = new XMLHttpRequest();
+  r.open("get", url, false);
+  r.send();
+  if(window.JSON.parse)
+  {
+    var model = window.JSON.parse(r.responseText);
+    if(model.vertecies && model.indecies && (model.indecies.length % 3) == 0)
+      return model;
+    else
+      console.debug("Invalid model format");
+  }
+  else
+  {
+    console.debug("Your browser doesn't support native JSON parsing, go away.");
+    return null;
+  }
+}
+
 var renderer = new Object();
+
+var turn_table = new Object();
 
 function main() {
   renderer.canvas = document.getElementById("main_canvas");
@@ -182,27 +274,27 @@ function main() {
   renderer.position = [0,0,5];
 
   renderer.ccw_mode = "CCW_3D";
-  //renderer.marker_mode = false;
+  renderer.marker_mode = true;
 
   renderer.init();
-
+  renderer.draw();
   window.requestAnimFrame(update, renderer.canvas);
 
-  var m = 
-  [[2,0,0],
-   [0,2,0],
-   [0,0,2]];
+  // var m = 
+  // [[2,0,0],
+  //  [0,2,0],
+  //  [0,0,2]];
 
-  var m2 = 
-  [[1,0,0],
-   [0,2,0],
-   [0,0,1]];
+  // var m2 = 
+  // [[1,0,0],
+  //  [0,2,0],
+  //  [0,0,1]];
 
-  //console.log(matrix_mul(m,m2));
+  //  console.log(m.m_transpose());
 
   //console.log(vector_transform(m, [1,2,3]));
 
-  console.log(median([[1,2,3], [5,6,7]], 3));
+  // console.log(median([[1,2,3], [5,6,7]], 3));
 
   // renderer.canvas.onmousedown = function(ev) { 
   //   console.debug("click");
@@ -244,9 +336,12 @@ function main() {
   console.debug("Hello");
 }
 
+turn_table.update = function() {
+
+}
+
 function update(){
-  //console.log("!!");
-  renderer.rotation = vector_add(renderer.rotation, [0.01,0.01,0.01]);
+  renderer.rotation = renderer.rotation.v_add_v([0.0,0.01,0.0]);
   window.requestAnimFrame(update, renderer.canvas); 
   renderer.draw();
 }
@@ -254,15 +349,18 @@ function update(){
 renderer.init = function(){
   this.aspect = this.canvas.width/this.canvas.height;
   // set defaults 
-  this.color = this.color || "#ffffff";
-  this.focal_distance = this.focal_distance || 1.0;
-  this.near_plane = this.near_plane || 1.0;
-  this.far_plane = this.far_plane || 1000.0;
-  this.indecies = this.indecies || [];
-  this.vertecies = this.vertecies || [];
-  this.rotation = this.rotation || [0,0,0];
-  this.position = this.position || [0,0,5];
-  this.marker_mode = this.marker_mode == null;
+  this.color = Fallback(this.color, "#ffffff");
+  this.focal_distance = Fallback(this.focal_distance, 1.0);
+  this.near_plane = Fallback(this.near_plane, 1.0);
+  this.far_plane = Fallback(this.far_plane, 1000.0);
+  this.indecies = Fallback(this.indecies, []);
+  this.vertecies = Fallback(this.vertecies, []);
+  this.rotation = Fallback(this.rotation, [0,0,0]);
+  this.position = Fallback(this.position, [0,0,5]);
+  this.scale = Fallback(this.scale, 1.0);
+  this.marker_mode = Fallback(this.marker_mode, true);
+  this.marker_culled_color = Fallback(this.marker_culled_color, "#ff0000");
+  this.marker_non_culled_color = Fallback(this.marker_non_culled_color, "#ffffff");
 }
 
 renderer.clear = function(){
@@ -271,38 +369,73 @@ renderer.clear = function(){
 }
 
 renderer.draw = function() {
-   // clean canvas
-    this.clear();
+  var d = this.focal_distance;
+  var np = this.near_plane;
+  var fp = this.far_plane;
+  var a = this.aspect;
+
+  var xform_ndc = function(v) {
+    var x = v[0];
+    var y = v[1];
+    var z = v[2];
+    var out = [d*x/(z*a), d*y/z, (z-np)/(fp-np)];
+    return out;
+  };
+
+  var RX = RotationX(this.rotation[0]);
+  var RY = RotationY(this.rotation[1]);
+  var RZ = RotationZ(this.rotation[2]);
+  var T = Translation(this.position);
+  var S = Scale(this.scale);
+  var M = T.m_mul_m(S).m_mul_m(RZ).m_mul_m(RY).m_mul_m(RX);
+  var xform_object = function(v) {
+    return M.m_mul_v([v[0], v[1], v[2], 1]);
+  };
+  
+  var w = this.canvas.width;
+  var h = this.canvas.height;
+  var xform_screen = function(v) {
+    return [w/2 + v[0]*w, h/2 - v[1]*h, v[2]];
+  };                   //     ^
+                       //     |_ This is the reflection in screen space - 
+                       //        that's why CCW2D expects a single reflection
+
+  // clear canvas
+  this.clear();
+
   var context = this.canvas.getContext("2d");
   context.strokeStyle = this.color;
 
   for (var i = 0; i < this.indecies.length; i+=3) {
     var culled = false;
 
-    var triangle = [this.vertecies[this.indecies[i]].concat(1), 
-                    this.vertecies[this.indecies[i+1]].concat(1), 
-                    this.vertecies[this.indecies[i+2]].concat(1)];
-    
+    var triangle = [this.vertecies[this.indecies[i]], 
+                    this.vertecies[this.indecies[i+1]], 
+                    this.vertecies[this.indecies[i+2]]];
+ 
     // apply model transformation matrix
-    triangle = this.xform_object(triangle);
-    context.fillStyle = this.color;
+    triangle = map(xform_object, triangle);
 
     // calculate marker position
     var marker;
     if(this.marker_mode) {
       marker = median(triangle, 3);
-      marker = this.xform_screen(this.xform_ndc([marker]))[0];
+      marker = xform_screen(xform_ndc(marker));
     }
 
-    culled = culled || this.ccw_mode == "CCW_3D" && !isCCW3D(triangle);
-
+    if(this.ccw_mode == "CCW_3D") {
+      culled = !isCCW3D(triangle);
+    }
+    
     // convert points to clip space
-    triangle = this.xform_ndc(triangle);
+    triangle = map(xform_ndc, triangle);
 
     // convert points to screen space
-    triangle = this.xform_screen(triangle);
+    triangle = map(xform_screen, triangle);
 
-    culled = culled || this.ccw_mode != "CCW_3D" && !isCCW2D(triangle); 
+    if(this.ccw_mode != "CCW_3D") {
+      culled = !isCCW2D(triangle);
+    }
 
     // draw the triangle
     if(!culled || this.marker_mode) {
@@ -318,8 +451,9 @@ renderer.draw = function() {
       context.stroke();
     }  
     
+    // draw markers
     if(this.marker_mode) {
-      context.fillStyle = culled ? "#ff0000" : this.color;
+      context.fillStyle = culled ? this.marker_culled_color : this.marker_non_culled_color;
       context.beginPath();
       context.arc(marker[0],marker[1],2,0*Math.PI,2*Math.PI);
       context.fill();
@@ -327,63 +461,4 @@ renderer.draw = function() {
   };
 }
 
-/* 
-  Take coordinates in view space and produces coordinates in clip space.
-  Although no clipping is done in this demo, it's still a useful logical step before
-  producing final screen coordinates. Think of this function as taking the whole 3d space
-  that contains imaginary frustum and squashing it into a cube with sides from -1 to 1.
-
-// [Vector] -> [Vector] */
-renderer.xform_ndc = function(vs) {
-  var vs_out = [];
-  var d = this.focal_distance;
-  var np = this.near_plane;
-  var fp = this.far_plane;
-  var a = this.aspect;
-  for (var i = 0; i < vs.length; i++) {
-    var x = vs[i][0];
-    var y = vs[i][1];
-    var z = vs[i][2];
-    // Apply perspective distortion to x and y,
-    // normalize frustum to a perfect cube -1 to 1 on every side   
-    vs_out[i] = [d*x/(z*a), d*y/z, (z-np)/(fp-np)];
-  };
-  return vs_out;
-}
-
-/* 
-  Constructs a matrix for model transformation and applies it
-
-  [Vector] -> [Vector] */
-renderer.xform_object = function(vs) {
-  var r = this.rotation;
-  var RX = RotationX(r[0]);
-  var RY = RotationY(r[1]);
-  var RZ = RotationZ(r[2]);
-  var RXYZ = matrix_mul(matrix_mul(RX, RY), RZ);
-  var T = Translation(this.position);
-  var vs_out = [];
-  for (var i = 0; i < vs.length; i++) {
-    vs_out[i] = vector_xfrom(RXYZ, vs[i]);
-    vs_out[i] = vector_xfrom(T, vs_out[i]);
-  }
-  return vs_out;
-}
-
-/* 
-  Takes vertecies in NDC space and stretches their x and y values to fit the screen
-  Ignores depth, but passes it through.
-
-  [Vector] -> [Vector] */
-renderer.xform_screen = function(vs) {
-  if(vs == null)
-    return null;
-  var w = this.canvas.width;
-  var h = this.canvas.height;
-  var vs_out = [];
-  for (var i = 0; i < vs.length; i++) {
-    vs_out[i] = [w/2 + vs[i][0]*w, h/2 - vs[i][1]*h, vs[i][2]];
-  };                              //        ^
-  return vs_out;                  //      This is the reflection in screen space - 
-}                                 //      that's why CCW2D expects a single reflection
 
